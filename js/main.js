f6b3e0bfaca7a3f31d80f33b2e110d8d54a332cf
FrancescoBenassi <francybenna77@gmail.com>
2024-10-09 22:47:30 +0200

    feat: added const

--- js/main.js
+++ js/main.js
@@ -1,59 +1,35 @@
 // Costanti dell'esercizio
 
+// Prezzo al km 
 const price = 0.21;
+// Anni over 
 const over = 65;
+// Anni under 
 const under = 18;
+// Sconto 40%
 const salesOver = 40 / 100;
+// Sconto al 20%
 const salesUnder = 20 / 100;
 
-//1) Chiedere il numero di km da percorrere
 
-const distance = prompt('Inserisci i km da percorre');
-console.log(distance);
+// Creare costante form 
 
-//2) Chiedere l'età del passeggero
+const calculatedPrice = document.getElementById('calculated-price');
 
-const age = prompt('Inserisci la tua età');
-console.log(age);
+// Chiedere il numero di km da percorrere
 
-//3) Validare che il numero non sia NaN
+const distance = document.getElementById('distance');
 
-if (isNaN(distance) || isNaN(age)) {
- alert('Errore');
-} else {
- console.log('Sono stati inseriti numeri nelle aree richieste, si può procedere con il calcolo del prezzo');
-}
+// Chiedere l'età del passeggero
 
-//4) Trasformare la stringa della distanza in numero
+const age = document.getElementById('age');
 
-const distanceNumber = Number(distance);
-console.log(distanceNumber);
 
-//5) Trasformare la stringa dell'età in numero
+calculatedPrice.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-const ageNumber = Number(age);
-console.log(ageNumber);
+  console.log('prova invio form');
 
-//6) Calcolare il prezzo del biglietto senza sconti (km * 0.21€)
-
-const priceFull = distanceNumber * price;
-console.log(priceFull);
-
-/*7) Applicare lo sconto in base all'età
-  8) Se il passeggero è minorenne avrà uno sconto del 20% altrimenti 
-se è over 65 avrà uno sconto del 40% altrimenti se non è minorenne e neanche over 65 non avrà nessun sconto */
-
-if (ageNumber > over) {
-  const priceSalesOver = priceFull - (priceFull * salesOver);
-  console.log(priceSalesOver);
-  const costSalesOver = `Il costo del biglietto è ${priceSalesOver.toFixed(2)}`;
-  console.log(costSalesOver);
-} else if (ageNumber < under) {
-  const priceSalesUnder = priceFull - (priceFull * salesUnder);
-  console.log(priceSalesUnder);
-  const costSalesUnder = `Il costo del biglietto è ${priceSalesUnder.toFixed(2)}`;
-  console.log(costSalesUnder);
-} else {
-  const costFull = `Il costo del biglietto è ${priceFull.toFixed(2)}`;
-  console.log(costFull);
-}
+  console.log('km ', distance.value);
+  console.log('age ', age.value);
+});
