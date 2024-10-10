6d2e616f0c2f4bdfa7f4f3de7c541643f7a39d76
FrancescoBenassi <francybenna77@gmail.com>
2024-10-10 10:58:21 +0200

    feat: added calculated price

---
+++ README.md
@@ -0,0 +1,23 @@
+# calcolo del prezzo del biglietto del treno
+
+Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. 
+
+1) Costanti dell'esercizio
+   - Prezzo al km
+   - Anni over
+   - Anni under 
+   - Sconto 40%
+   - Sconto al 20%
+
+2) Creare variabile form 
+3) Chiedere il numero di km da percorrere
+4) Chiedere l'età del passeggero
+5) Variabili risultato
+6) Aggiungere evento al 'submit'
+   - Trasformare la stringa in numero
+   - Stampare i numeri sulla console 
+   - Calcolare il prezzo intero 
+   - Calcolare il prezzo scontato se ha i requisiti
+   - Prezzo finale
+   - Reset dei campi input
+   - Modifica al DOM con l'aggiunta del prezzo finale
--- js/main.js
+++ js/main.js
@@ -1,35 +1,76 @@
-// Costanti dell'esercizio
+// 1) Costanti dell'esercizio
 
-// Prezzo al km 
+// - Prezzo al km 
 const price = 0.21;
-// Anni over 
+// - Anni over 
 const over = 65;
-// Anni under 
+// - Anni under 
 const under = 18;
-// Sconto 40%
+// - Sconto 40%
 const salesOver = 40 / 100;
-// Sconto al 20%
+// - Sconto al 20%
 const salesUnder = 20 / 100;
 
 
-// Creare costante form 
+// 2) Creare variabile form 
 
 const calculatedPrice = document.getElementById('calculated-price');
 
-// Chiedere il numero di km da percorrere
+// 3) Chiedere il numero di km da percorrere
 
 const distance = document.getElementById('distance');
 
-// Chiedere l'età del passeggero
+// 4) Chiedere l'età del passeggero
 
 const age = document.getElementById('age');
 
+// 5) Variabili risultato
+
+const resultConainer = document.getElementById('result-container');
+const result = document.getElementById('result');
+
+// 6) Aggiungere evento al 'submit'
 
 calculatedPrice.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  console.log('prova invio form');
+  // - Trasformare la stringa in numero
 
-  console.log('km ', distance.value);
-  console.log('age ', age.value);
+  const distanceNumber = parseFloat(distance.value);
+  const ageNumber = parseFloat(age.value);
+
+  // - Stampare i numeri sulla console 
+
+  console.log('km ', distanceNumber);
+  console.log('age ', ageNumber);
+
+  // - Calcolare il prezzo intero 
+
+  let priceTicket = distanceNumber * price;
+  console.log('prezzo intero ', priceTicket);
+
+  // - Calcolare il prezzo scontato se ha i requisiti
+
+  if (ageNumber > over) {
+    priceTicket = priceTicket - (priceTicket * salesOver);
+    console.log(priceTicket);
+  } else if (ageNumber < under) {
+    priceTicket = priceTicket - (priceTicket * salesUnder);
+    console.log(priceTicket);
+  }
+
+  // - Prezzo finale
+
+  const priceEnd = `Il costo del biglietto è ${priceTicket.toFixed(2)}`;
+  console.log(priceEnd);
+
+  // - Reset dei campi input
+
+  distance.value = '';
+  age.value = '';
+
+  // - Modifica al DOM con l'aggiunta del prezzo finale
+
+  result.innerHTML = `Il costo del biglietto è ${priceTicket.toFixed(2)} `;
+  resultConainer.classList.remove('d-none');
 });
